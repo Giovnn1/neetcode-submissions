0070ab81e610218e3e3008a71204835a52f0bfdf
Giovnn1 <130870338+Giovnn1@users.noreply.github.com>
2026-08-11 11:46:15 +0200

    Add: house-robber-ii - submission-7

---
+++ Data Structures & Algorithms/house-robber-ii/submission-7.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        #the idea is tha no legit path contains both nums[0] and nums[len(nums)-1], so we run the rob1 (see previous exercise) on nums[1:] and nums[:len(nums) - 1]
+        def rob1(n):
+            L = len(n)
+            if L <= 2:
+                return max(n)
+            m1, m2, m3 = n[L-1], n[L-2], n[L-3] + n[L-1]
+            for i in range(4, L+1):
+                m = n[L-i] + max(m2, m1)
+                m3, m2, m1 = m, m3, m2
+            return max(m3, m2)
+        
+        return max(rob1(nums[1:]), rob1(nums[:len(nums)-1])) if len(nums) > 2 else max(nums)
