7451faeb41820338b1ebf158b2570eaaa5685ccc
Giovnn1 <130870338+Giovnn1@users.noreply.github.com>
2026-08-31 12:25:21 +0200

    Add: jump-game - submission-8

---
+++ Data Structures & Algorithms/jump-game/submission-8.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        #This algorithm would need a cleaning, e.g. you don't need i_land, bc it's actually equal to s + jump
+        s = 0
+        while s < len(nums) - 1:
+            if nums[s] == 0:
+                return False
+            jump, i_land = -1, -1
+            for i in range(1, min(nums[s] + 1, len(nums) - s)):
+                if i + nums[s + i] > jump + nums[i_land] or (i + nums[s + i] == jump + nums[i_land] and i > jump):
+                    jump, i_land = i, s + i
+            s = i_land
+        
+        return True
+            
